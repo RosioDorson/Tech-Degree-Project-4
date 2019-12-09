cf4f168aa3ffd8d6031ebc773fed9d225fc51b74
Rosio <rosioilene@yahoo.com>
2019-12-08 22:40:41 -0800

    added beginning code

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,21 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+     constructor () {
+     this.missed = 0;
+     this.phrases = [
+        new Phrase("Toto Ive got a feeling we're not in Kansas anymore"),
+        new Phrase("May the Force be with you"),
+        new Phrase("My precious"),
+        new Phrase("Bond James Bond"),
+        new Phrase("Im king of the world"),
+     ];
+     this.activePhrase = null;
+    };
+    getRandomPhrase() {
+        const randomPhrase = Math.floor(Math.random() * 5);
+        return this.phrases[randomPhrase];
+    };
+ }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+
+//  const phrase = new Phrase();
+
+
+// const phrase = new Phrase('Life is good!');
+// console.log(`Phrase - phrase: ${}`);
+
+// const game = new Game();
+// game.phrases.forEach((phrase, index) => {
+//     console.log(`Phrase ${index} - phrase: ${phrase.phrase}`)
+// });
+
+const logPhrase = (phrase) => {
+    console.log(`Phrase - phrase: `, phrase.phrase);
+};
+
+const game = new Game();
+
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
